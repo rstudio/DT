5bd30496a4ba4f72bcad80338b74ddc41c6c62fd
Yihui Xie <xie@yihui.name>
2014-11-11 15:59:10 -0600

    tools to update DataTables in the future

---
+++ tools/update_DT.R
@@ -0,0 +1,24 @@
+owd = setwd('tools')
+ver = '1.10.3'
+out = sprintf('DataTables-%s.zip', ver)
+unlink('DataTables*', recursive = TRUE)
+download.file(sprintf('http://datatables.net/releases/DataTables-%s.zip', ver), out, mode = 'wb')
+unzip(out)
+
+# base64 encode images into CSS
+setwd(file.path(sprintf('DataTables-%s', ver), 'media', 'css'))
+x = readLines('jquery.dataTables.min.css')
+m = gregexpr('"[.][.][^"]+?[.]png"', x)
+regmatches(x, m) = lapply(regmatches(x, m), function(ps) {
+  ps = gsub('^"|"$', '', ps)
+  sapply(ps, knitr::image_uri)
+})
+writeLines(x, 'jquery.dataTables.min.css')
+setwd('..')
+
+# copy required files to the R package
+file.copy('css/jquery.dataTables.min.css', '../../../inst/lib/datatables/css/', overwrite = TRUE)
+file.copy('js/jquery.dataTables.min.js', '../../../inst/lib/datatables/js/', overwrite = TRUE)
+file.copy('../license.txt', '../../../inst/lib/datatables/', overwrite = TRUE)
+
+setwd(owd)
