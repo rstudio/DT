cd73b03173a2ec19f7f22f2b5515d2d029802f37
Yihui Xie <xie@yihui.name>
2014-12-02 23:00:26 -0600

    so now we use JS() instead of I()

--- inst/examples/datatable.R
+++ inst/examples/datatable.R
@@ -15,31 +15,31 @@ datatable(mtcars, options = list(order = list(list(1, 'asc'), list(3, 'desc'))))
 # the initComplete callback
 datatable(iris, options = list(
   order = list(),
-  initComplete = I(c(
+  initComplete = JS(
     "function(settings, json) {",
     "alert('Initialization complete!');",
-    "}"))
+    "}")
 ))
 
 # if the data is wider than 6 chars, use first 6 + ...
 datatable(iris, options = list(order = list(), columnDefs = list(list(
   targets = 4,
-  render = I(c(
+  render = JS(
     "function ( data, type, full, meta ) {",
     "return type === 'display' && data.length > 6 ?",
     "'<span title=\"'+data+'\">'+data.substr( 0, 6 )+'...</span>' : data;",
-    "}"))
+    "}")
 ))))
 
 # display some big numbers, right-align them, and format the 3rd column
 m = as.data.frame(matrix(rnorm(100, 1e5, 1e6), 20))
 datatable(m, options = list(
   columnDefs = list(list(className = 'dt-right', targets = '_all')),
-  rowCallback = I(c(
+  rowCallback = JS(
     "function(row, data) {",
     "var num = '$' + data[2].toString().replace(/\\B(?=(\\d{3})+(?!\\d))/g, ',');",
     "$('td:eq(2)', row).html(num);",
-    "}"))
+    "}")
 ))
 
 
