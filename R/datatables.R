a178208619d3eee7c4655a8ba6f69be159d18845
Yihui Xie <xie@yihui.name>
2014-12-02 22:57:39 -0600

    the JS() feature has been migrated to htmlwidgets, so a lot simpler in DT now

--- R/datatables.R
+++ R/datatables.R
@@ -30,8 +30,7 @@ datatable = function(data, id = NULL, options = list(order = list())) {
     data = if (isDF) unname(as.list(data)) else unname(data)
   }
   params = list(
-    data = data, isDF = isDF, table = as.character(table),
-    evals = options_evals(options), options = options_sanitize(options)
+    data = data, isDF = isDF, table = as.character(table), options = options
   )
 
   htmlwidgets::createWidget('datatables', params, package = 'DT')
@@ -45,38 +44,4 @@ fix_WAT = function(data) {
   # toJSON(list(x = matrix(nrow = 0, ncol = 1))) => {"x": } (shiny#299)
   if (is.matrix(data) && nrow(data) == 0) return(list())
   data
-}
-
-# JSON elements that are character with the class AsIs will be evaluated
-options_sanitize = function(options) {
-  if (!is.list(options) || length(options) == 0) return(options)
-  lapply(options, function(x) {
-    if (is.list(x)) return(options_sanitize(x))
-    if (is.character(x) && inherits(x, 'AsIs')) x = paste(x, collapse = '\n')
-    x
-  })
-}
-
-should_eval = function(options) {
-  if (is.list(options)) {
-    if ((n <- length(options)) == 0) return(FALSE)
-    # use numeric indices as names (remember JS indexes from 0, hence -1 here)
-    if (is.null(nms <- names(options))) nms = names(options) = seq_len(n) - 1L
-    if (length(nms) != n || any(nms == ''))
-      stop("'options' must be a fully named list, or have no names (NULL)")
-    lapply(options, should_eval)
-  } else {
-    is.character(options) && inherits(options, 'AsIs')
-  }
-}
-
-# Does this: list(foo = list(1, list(bar = I('function(){}')), 2)) => foo.2.bar
-# Later on the JS side, we will split foo.2.bar to ['foo', '2', 'bar'] and
-# evaluate the JSON object member. Note '2' (character) should have been 2
-# (integer) but it does not seem to matter in JS: x[2] is the same as x['2']
-# when all child members of x are unnamed, and ('2' in x) will be true even if x
-# is an array without names. This is a little hackish.
-options_evals = function(options) {
-  evals = names(which(unlist(should_eval(options))))
-  I(evals)  # need I() to prevent RJSONIO::toJSON() from converting it to scalar
 }
