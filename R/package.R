b6ce49b0fcd5f61c3144402e0542935d2aeb7d3c
Yihui Xie <xie@yihui.name>
2014-12-02 23:00:04 -0600

    the magic to export JS from htmlwidgets without writing doc for it

---
+++ R/package.R
@@ -0,0 +1,18 @@
+## import and export functions from other packages; also automatically generate
+## Rd for them to avoid R CMD check NOTE's
+
+importRd = function(names, package) {
+  library(package, character.only = TRUE)
+  for (name in names) suppressMessages(utils::promptImport(
+    name, name = name, importedFrom = package
+  ))
+}
+
+#' @importFrom htmlwidgets JS
+#' @export JS
+if (file_test('-d', 'man')) local({
+  owd = setwd('man'); on.exit(setwd(owd))
+  importRd('JS', 'htmlwidgets')
+})
+
+rm(importRd)
