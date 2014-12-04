dde823d26b73972017e3325e21428818d3011fb9
Yihui Xie <xie@yihui.name>
2014-12-04 16:29:28 -0600

    let Rd2roxygen::importRd() handle the doc for re-exported objects automatically

--- R/package.R
+++ R/package.R
@@ -1,29 +1,5 @@
-## import and export functions from other packages; also automatically generate
-## Rd for them to avoid R CMD check NOTE's
-
-importRd = function(names, package) {
-  for (name in names) suppressMessages(utils::promptImport(
-    NULL, name = gsub('%', '\\\\%', name), importedFrom = package,
-    filename = importFilename(name)
-  ))
-}
-
-# make sure the filename is valid
-importFilename = function(name) {
-  if (!grepl('^[A-Za-z0-9._-]+$', name))
-    name = paste(as.character(charToRaw(name)), collapse = '')
-  paste0(name, '.Rd')
-}
-
 #' @importFrom htmlwidgets JS
 #' @export JS
 #' @importFrom magrittr %>%
 #' @export %>%
-if (file_test('-d', 'man')) local({
-  owd = setwd('man'); on.exit(setwd(owd))
-  importRd('JS', 'htmlwidgets')
-  importRd('%>%', 'magrittr')
-})
-
-rm(importRd)
-rm(importFilename)
+NULL
