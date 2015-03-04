4fc326a0634b40aa45a57ed621f2fa8a16a70797
Yihui Xie <xie@yihui.name>
2015-03-04 11:34:53 -0600

    add a scroller example in shiny

---
+++ inst/examples/DT-scroller/server.R
@@ -0,0 +1,27 @@
+library(shiny)
+
+if (!file_test('-d', 'www')) dir.create('www')
+
+# prepare the Ajax data source
+writeLines(
+  RJSONIO::toJSON(list(aaData = matrix(1:4000, ncol = 4))),
+  'www/large.txt'
+)
+
+shinyServer(function(input, output) {
+  # an empty matrix to generate the table container
+  sketch = matrix(ncol = 4, dimnames = list(NULL, head(letters, 4)))
+  # render the widget
+  output$tbl = DT::renderDataTable(
+    DT::datatable(
+      sketch, extensions = 'Scroller',
+      options = list(
+        ajax = 'large.txt',
+        deferRender = TRUE,
+        dom = 'frtiS',
+        scrollY = 200,
+        scrollCollapse = TRUE
+      )
+    )
+  )
+})
