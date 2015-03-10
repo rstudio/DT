cddf5c36c2dcf0f82356a80ff80ee2eeda43178f
Yihui Xie <xie@yihui.name>
2015-03-09 23:31:12 -0500

    specify initialization options for extensions

--- libs/datatables-binding/datatables.js
+++ libs/datatables-binding/datatables.js
@@ -12,21 +12,31 @@ HTMLWidgets.widget({
       data: cells
     };
     var table = $el.find('table').DataTable($.extend(options, data.options || {}));
+    // initialize extensions
+    for (var ext in data.extOptions) {
+      new $.fn.dataTable[ext](table, data.extOptions[ext] || {});
+    }
+    // run the callback function on the table instance
     if (typeof data.callback === 'string') {
       var callback = eval('(' + data.callback + ')');
       if (typeof callback === 'function') callback(table);
     }
+    // interaction with shiny
     if (!window.Shiny) return;
-    var selectedRows = [];
-    table.$('input[type="checkbox"].DT.checkboxRows').on('change', function() {
-      var $this = $(this), value = $this.data('row');
-      if (this.checked) {
-        selectedRows.push(value);
-      } else {
-        selectedRows.splice($.inArray(value, selectedRows), 1);
-      }
-      Shiny.onInputChange(el.id + '_selected', selectedRows);
-    });
-    Shiny.onInputChange(el.id + '_selected', selectedRows);
+    var selected = [];
+    table.$('input[type="checkbox"].DT.checkboxRows')
+      .each(function(i) {
+        if (this.checked) selected.push($(this).data('row'));
+      })
+      .on('change', function() {
+        var $this = $(this), value = $this.data('row');
+        if (this.checked) {
+          selected.push(value);
+        } else {
+          selected.splice($.inArray(value, selected), 1);
+        }
+        Shiny.onInputChange(el.id + '_selected', selected);
+      });
+    Shiny.onInputChange(el.id + '_selected', selected);
   }
 })
