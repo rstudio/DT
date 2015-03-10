dac83c2503c043fec4a2c001ae71d8205a1bb8a5
Yihui Xie <xie@yihui.name>
2015-03-10 15:35:30 -0500

    changes in DT

--- libs/datatables-binding/datatables.js
+++ libs/datatables-binding/datatables.js
@@ -23,6 +23,10 @@ HTMLWidgets.widget({
     }
     // interaction with shiny
     if (!window.Shiny) return;
+    var changeInput = function(id, data) {
+      Shiny.onInputChange(el.id + '_' + id, data);
+    };
+    // selected rows (checkboxes added via DT::checkboxRows())
     var selected = [];
     table.$('input[type="checkbox"].DT.checkboxRows')
       .each(function(i) {
@@ -35,8 +39,25 @@ HTMLWidgets.widget({
         } else {
           selected.splice($.inArray(value, selected), 1);
         }
-        Shiny.onInputChange(el.id + '_selected', selected);
+        changeInput('selected', selected);
       });
-    Shiny.onInputChange(el.id + '_selected', selected);
+    changeInput('selected', selected);
+    // expose some table info to Shiny
+    var updateTableInfo = function(e, settings) {
+      // TODO: is anyone interested in the page info?
+      // changeInput('page_info', table.page.info());
+      var updateRowInfo = function(id, modifier) {
+        changeInput('rows' + '_' + id, table.rows($.extend({
+          search: 'applied',
+          page: 'all'
+        }, modifier)).indexes().map(function(i) {
+          return 1 + i;
+        }).toArray());
+      };
+      updateRowInfo('current', {page: 'current'});
+      updateRowInfo('all', {});
+    };
+    table.on('draw.dt', updateTableInfo);
+    updateTableInfo();
   }
 })
