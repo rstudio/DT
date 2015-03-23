fa61692b58799d21f5d8a7d8d91a5c00af955c42
Yihui Xie <xie@yihui.name>
2015-03-23 13:53:12 -0500

    document dataTableAjax()

---
+++ inst/examples/ajax-shiny.R
@@ -0,0 +1,21 @@
+# !formatR
+DTApp = function(data, ..., options = list()) {
+  library(shiny)
+  shinyApp(
+    ui = fluidPage(
+      title = 'Server-side processing of DataTables',
+      fluidRow(
+        DT::dataTableOutput('tbl')
+      )
+    ),
+    server = function(input, output, session) {
+      options$ajax = list(url = dataTableAjax(session, data))
+      # create a widget using an Ajax URL created above
+      widget = datatable(data, server = TRUE, ..., options = options)
+      output$tbl = DT::renderDataTable(widget)
+    }
+  )
+}
+
+if (interactive()) DTApp(iris)
+if (interactive()) DTApp(iris, filter = 'top')
