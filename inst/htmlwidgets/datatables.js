a6b77e956d057a204f5ca48eb6f8a93ee901662a
Yihui Xie <xie@yihui.name>
2015-03-23 12:13:35 -0500

    all filters use search boxes initially, and the boxes will be expanded to the real filters (sliders or selectize) on click

--- inst/htmlwidgets/datatables.js
+++ inst/htmlwidgets/datatables.js
@@ -31,44 +31,117 @@ HTMLWidgets.widget({
     }
 
     if (data.filter !== 'none') {
+
       var filterRow;
       if (data.filter === 'top') {
         filterRow = $(table.table().node()).find('thead tr:last td');
       } else {
         filterRow = $(table.columns().footer());
       }
+
       filterRow.each(function(i, td) {
 
         var $td = $(td), type = $td.data('type'), filter;
-        if (type === 'category') {
-          filter = $td.children('select').selectize({
+        var $input = $td.children('div').first().children('input');
+        $input.on('input blur', function() {
+          $input.next('span').toggle(Boolean($input.val()));
+        });
+        // Bootstrap sets pointer-events to none and we won't be able to click
+        // the clear button
+        $input.next('span').css('pointer-events', 'auto').hide().click(function() {
+          $(this).hide().prev('input').val('').trigger('input').focus();
+        });
+        var $x = $td.children('div').last();
+
+        if (type === 'factor') {
+          $input.on({
+            click: function() {
+              $input.parent().hide(); $x.show(); filter[0].selectize.focus();
+            },
+            input: function() {
+              if ($input.val() === '') filter[0].selectize.setValue([]);
+            }
+          });
+          filter = $x.children('select').selectize({
             plugins: ['remove_button'],
+            hideSelected: true,
             onChange: function(value) {
+              $input.val(value === null ? '' : JSON.stringify(value));
+              if (value) $input.trigger('input');
+              $input.attr('title', $input.val());
               if (server) {
-                table.column(i).search(value || '').draw();
+                table.column(i).search(value ? JSON.stringify(value) : '').draw();
                 return;
               }
               // turn off filter if nothing selected
               $td.data('filter', value !== null && value.length > 0);
               table.draw();  // redraw table, and filters will be applied
+            },
+            onBlur: function() {
+              $x.hide(); $input.parent().show();
             }
           });
-        } else if (type === 'number' || type === 'date') {
-          var $x = $td.children('div');
+        } else if (type === 'character') {
+          var fun = function() {
+            table.column(i).search($input.val()).draw();
+          };
+          if (server) {
+            fun = throttle(fun, 1000);
+          }
+          $input.on('input', fun);
+        } else if (type === 'number' || type === 'date' || type === 'time') {
           var r1 = +$x.data('min'), r2 = +$x.data('max');
-          filter = $x.noUiSlider({
+          $input.on({
+            focus: function() {
+              $x.show();
+            },
+            blur: function() {
+              $x.hide();
+            },
+            input: function() {
+              if ($input.val() === '') filter.val([r1, r2]);
+            },
+            change: function() {
+              var v = $input.val().split('...');
+              if (v.length !== 2) return;
+              filter.val([+v[0], +v[1]]);
+            }
+          });
+          var formatDate = function(d) {
+            var x = new Date(+d);
+            if (type === 'date') {
+              return x.getFullYear() + '-' + ('0' + (1 + x.getMonth())).substr(-2, 2)
+                      + '-' + x.getDate();
+            } else {
+              return x.toISOString();
+            }
+          };
+          var opts = type === 'date' ? { step: 24 * 60 * 60 * 1000 } : {};
+          filter = $x.noUiSlider($.extend({
             start: [r1, r2],
             range: {min: r1, max: r2},
             connect: true
-          }).on({
-            change: function() {
+          }, opts)).on({
+            set: function() {
               var val = $(this).val();
-              if (server) {
-                table.column(i).search(val.join(',')).draw();
-                return;
-              }
               // turn off filter if in full range
               $td.data('filter', val[0] != r1 || val[1] != r2);
+              if ($td.data('filter')) {
+                var ival;
+                if (type == 'number') {
+                  ival = val.join(' ... ');
+                } else {
+                  var v1 = formatDate(val[0]), v2 = formatDate(val[1]);
+                  ival = v1 + ' ... ' + v2;
+                }
+                $input.attr('title', ival).val(ival).trigger('input');
+              } else {
+                $input.attr('title', '').val('');
+              }
+              if (server) {
+                table.column(i).search($td.data('filter') ? val.join(',') : '').draw();
+                return;
+              }
               table.draw();
             }
           });
