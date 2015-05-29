f65dd21eb8e0210f9f1afa74d9bced449f07b84b
Yihui Xie <xie@yihui.name>
2015-05-29 11:50:20 -0500

    new functions formatStyle(), styleInterval(), styleEqual(), and styleColorBar()
    inspired by https://github.com/renkun-ken/formattable (although not as fancy as formattable) and thanks @renkun-ken
    
    closes #31

--- R/format.R
+++ R/format.R
@@ -12,8 +12,10 @@ formatColumns = function(table, columns, template, ...) {
 
 #' Format table columns
 #'
-#' Format numeric columns in a table as currency or percentages, or round
-#' numbers to a specified number of decimal places.
+#' Format numeric columns in a table as currency (\code{formatCurrency()}) or
+#' percentages (\code{formatPercentage()}), or round numbers to a specified
+#' number of decimal places (\code{formatRound()}). The function
+#' \code{formatStyle()} applies CSS styles to table cells by column.
 #' @param table a table object created from \code{\link{datatable}()}
 #' @param columns the indices of the columns to be formatted (can be character,
 #'   numeric, logical, or a formula of the form \code{~ V1 + V2}, which is
@@ -27,7 +29,8 @@ formatColumns = function(table, columns, template, ...) {
 #' @references See \url{http://rstudio.github.io/DT/functions.html} for detailed
 #'   documentation and examples.
 #' @export
-#' @examples library(DT)
+#' @examples # !formatR
+#' library(DT)
 #' m = cbind(matrix(rnorm(120, 1e5, 1e6), 40), runif(40), rnorm(40, 100))
 #' colnames(m) = head(LETTERS, ncol(m))
 #' m
@@ -37,6 +40,16 @@ formatColumns = function(table, columns, template, ...) {
 #'
 #' # the first two columns are Euro currency, and round column E to 3 decimal places
 #' datatable(m) %>% formatCurrency(1:2, '\U20AC') %>% formatRound('E', 3)
+#'
+#' # apply CSS styles to columns
+#' datatable(iris)
+#'   %>% formatStyle(~Sepal.Length, list(
+#'     `font-weight` = styleInterval(5, c('bold', 'weight'))
+#'   ))
+#'   %>% formatStyle(~Sepal.Width, list(
+#'     color = styleInterval(3.4, c('red', 'white')),
+#'     `background-color` = styleInterval(3.4, c('yellow', 'gray'))
+#'   ))
 formatCurrency = function(table, columns, currency = '$', interval = 3, mark = ',') {
   formatColumns(table, columns, tplCurrency, currency, interval, mark)
 }
@@ -58,6 +71,16 @@ formatRound = function(table, columns, digits = 2) {
 #' @rdname formatCurrency
 formatDate = function(table, columns, method = 'toDateString') {
   formatColumns(table, columns, tplDate, method)
+}
+
+#' @param styles a list of CSS styles, e.g. \code{list(color = 'red',
+#'   `font-weight` = 'bold')}; if you want to condition CSS styles on the cell
+#'   values, you may use the helper functions such as
+#'   \code{\link{styleInterval}()}
+#' @export
+#' @rdname formatCurrency
+formatStyle = function(table, columns, styles = list()) {
+  formatColumns(table, columns, tplStyle, styles)
 }
 
 # turn character/logical indices to numeric indices
@@ -117,3 +140,91 @@ DateMethods = c(
   'toDateString', 'toISOString', 'toLocaleDateString', 'toLocaleString',
   'toLocaleTimeString', 'toString', 'toTimeString', 'toUTCString'
 )
+
+tplStyle = function(cols, styles) {
+  if (length(styles) == 0) return()
+  if (!is.list(styles)) stop("'styles' must be a list")
+  JSclass = class(JS(''))
+  # if a style is specified in JS(), do not single quote it (it is a JS expression)
+  styles = vapply(styles, function(s) {
+    isJS = inherits(s, JSclass)
+    s = paste(s, collapse = '\n')
+    if (isJS) s else sprintf("'%s'", s)
+  }, FUN.VALUE = character(1))
+  css = paste(sprintf("'%s':%s", names(styles), styles), collapse = ',')
+  sprintf(
+    "var value=data[%s]; $(this.api().cell(row, %s).node()).css({%s});",
+    cols, cols, css
+  )
+}
+
+#' Conditional CSS styles
+#'
+#' A few helper functions for the \code{\link{formatStyle}()} function to
+#' calculate CSS styles for table cells based on the cell values. Under the
+#' hood, they just generate JavaScript and CSS code from the values specified in
+#' R.
+#'
+#' The function \code{styleInterval()} maps intervals to CSS values. Its
+#' argument \code{values} must be of length \code{n + 1} where \code{n =
+#' length(cuts)}. The right-closed interval \samp{(cuts[i - 1], cuts[i]]} is
+#' mapped to \samp{values[i]} for \samp{i = 2, 3, ..., n}; \samp{values[1]} is
+#' for the interval \samp{(-Inf, cuts[1]]}, and \samp{values[n + 1]} is for
+#' \samp{(cuts[n], +Inf)}. You can think of the order of \code{cuts} and
+#' \code{values} using this diagram: \samp{-Inf -> values[1] -> cuts[1] ->
+#' values[2] -> cuts[2] -> ... -> values[n] -> cuts[n] -> values[n + 1] ->
+#' +Inf}.
+#'
+#' The function \code{styleEqual()} maps data values to CSS values in the
+#' one-to-one manner, i.e. \code{values[i]} is used when the table cell value is
+#' \code{levels[i]}.
+#'
+#' The function \code{styleColorBar()} can be used to draw background color bars
+#' behind table cells in a column, and the width of bars is proportional to the
+#' column values.
+#' @param cuts a vector of cut points (sorted increasingly)
+#' @param values a vector of CSS values
+#' @export
+styleInterval = function(cuts, values) {
+  n = length(cuts)
+  if (n != length(values) - 1)
+    stop("length(cuts) must be equal to length(values) - 1")
+  values = sprintf("'%s'", values)
+  if (n == 0) return(values)
+  if (!all(cuts == sort(cuts))) stop("'cuts' must be sorted increasingly")
+  js = ''
+  for (i in seq_len(n)) {
+    js = paste0(js, sprintf('value <= %s ? %s : ', cuts[i], values[i]))
+  }
+  JS(paste0(js, values[n + 1]))
+}
+
+#' @param levels a character vector of data values to be mapped (one-to-one) to
+#'   CSS values
+#' @export
+#' @rdname styleInterval
+styleEqual = function(levels, values) {
+  n = length(levels)
+  if (n != length(values))
+    stop("length(levels) must be equal to length(values)")
+  if (n == 0) return("''")
+  js = ''
+  for (i in seq_len(n)) {
+    js = paste0(js, sprintf("value == '%s' ? '%s' : ", levels[i], values[i]))
+  }
+  JS(paste0(js, "''"))
+}
+
+#' @param data the numeric vector to be represented as color bars (in fact, only
+#'   its range, i.e. min and max, is needed here)
+#' @param color the color of the bars
+#' @export
+#' @rdname styleInterval
+styleColorBar = function(data, color) {
+  rg = range(data, na.rm = TRUE, finite = TRUE)
+  r1 = rg[1]; r2 = rg[2]; r = r2 - r1
+  JS(sprintf(
+    "isNaN(parseFloat(value)) || value == %s ? '' : 'linear-gradient(90deg, transparent ' + (%s - value)/%s * 100 + '%%, %s ' + (%s - value)/%s * 100 + '%%)'",
+    r1, r2, r, color, r2, r
+  ))
+}
