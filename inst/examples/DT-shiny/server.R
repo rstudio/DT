38f4f52fcfe6c01806196a126eaaa3eebda45cd9
Joe Cheng <joe@rstudio.org>
2015-06-02 16:23:57 -0700

    The default for renderDataTable(server=...) has changed; fix example

--- inst/examples/DT-shiny/server.R
+++ inst/examples/DT-shiny/server.R
@@ -2,6 +2,6 @@ library(shiny)
 library(DT)
 
 shinyServer(function(input, output, session) {
-  output$tbl_a = DT::renderDataTable(iris)
-  output$tbl_b = DT::renderDataTable(iris, server = TRUE)
+  output$tbl_a = DT::renderDataTable(iris, server = FALSE)
+  output$tbl_b = DT::renderDataTable(iris)
 })
