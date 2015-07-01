01cc966df39a92eef3ca59915e6f485b6f5799f8
Yihui Xie <xie@yihui.name>
2015-06-30 23:57:36 -0500

    add a shiny example of using the value of clicked cells

---
+++ inst/examples/DT-click/server.R
@@ -0,0 +1,27 @@
+library(shiny)
+library(DT)
+
+shinyServer(function(input, output, session) {
+
+  # add CSS style 'cursor: pointer' to the 0-th column (i.e. row names)
+  output$x1 = DT::renderDataTable({
+    datatable(cars) %>% formatStyle(0, cursor = 'pointer')
+  })
+
+  observe({
+    info = input$x1_cell_clicked
+    # do nothing if not clicked yet, or the clicked cell is not in the 1st column
+    if (is.null(info$value) || info$column != 0) return()
+    updateTabsetPanel(session, 'x0', selected = 'Plot')
+    updateTextInput(session, 'x2', value = info$value)
+  })
+
+  # highlight the point of the selected cell
+  output$x3 = renderPlot({
+    par(mar = c(4, 4, 1, .1))
+    plot(cars)
+    id = input$x2
+    if (id != '') points(cars[id, , drop = FALSE], pch = 19, cex = 2)
+  })
+
+})
