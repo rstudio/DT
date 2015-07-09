fe8c6a47715da80abbe44c2be8f46bf9990c1cd4
Yihui Xie <xie@yihui.name>
2015-07-09 17:17:04 -0500

    an example of DT manipulation methods

---
+++ inst/examples/DT-proxy/Readme.md
@@ -0,0 +1,2 @@
+You can pass a `dataTableProxy()` object to several methods to manipulate an
+existing table without completely re-rendering it.
---
+++ inst/examples/DT-proxy/server.R
@@ -0,0 +1,39 @@
+library(shiny)
+library(DT)
+
+shinyServer(function(input, output, session) {
+
+  # using server = FALSE mainly for addRow(); server = TRUE works for
+  # selectRows() and selectColumns() (for selectRows() + server = TRUE, don't
+  # use as.numeric() below)
+  output$foo = DT::renderDataTable(
+    iris, server = FALSE, selection = list(target = 'row+column')
+  )
+
+  proxy = dataTableProxy('foo')
+
+  observeEvent(input$select1, {
+    selectRows(proxy, as.numeric(input$rows))
+  })
+
+  observeEvent(input$select2, {
+    selectColumns(proxy, input$col)
+  })
+
+  observeEvent(input$clear1, {
+    selectRows(proxy, NULL)
+  })
+
+  observeEvent(input$clear2, {
+    selectColumns(proxy, NULL)
+  })
+
+  observeEvent(input$add, {
+    addRow(proxy, iris[sample(nrow(iris), 1), , drop = FALSE])
+  })
+
+  output$info = renderPrint({
+    list(rows = input$foo_rows_selected, columns = input$foo_columns_selected)
+  })
+
+})
