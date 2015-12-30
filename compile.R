249440c40f0c423f41f0cb3a6eab289fa2341e8d
Yihui Xie <xie@yihui.name>
2015-12-30 13:55:45 -0600

    use the much cleaner options(htmltools.dir.version = FALSE) and removed the old ugly hack to get rid of version numbers

--- compile.R
+++ compile.R
@@ -1,18 +1,5 @@
 set.seed(0102)
-options(htmlwidgets.TOJSON_ARGS = list(pretty = TRUE))
+options(htmlwidgets.TOJSON_ARGS = list(pretty = TRUE), htmltools.dir.version = FALSE)
 Sys.setenv(R_KNITR_OPTIONS = 'knitr.chunk.tidy=FALSE')
 library(DT)
-f = rmarkdown::render(commandArgs(TRUE))
-# remove version numbers in HTML
-r = '-\\d+[.]\\d+([.]\\d+)?$'
-v1 = rev(list.files('libs', r, full.names = TRUE))
-v2 = gsub(r, '', v1)
-x = readLines(f)
-for (i in seq_along(v1)) {
-  x = gsub(v1[i], v2[i], x, fixed = TRUE)
-}
-# delete lower versions of libraries, and only use higher versions
-i = duplicated(v2)
-unlink(c(v1[i], v2), recursive = TRUE)
-file.rename(v1[!i], v2[!i])
-writeLines(x, f)
+rmarkdown::render(commandArgs(TRUE))
