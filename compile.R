0aef1403998e433bde81c2034e53d55dbb527c46
Yihui Xie <xie@yihui.name>
2016-03-19 17:10:26 -0500

    remove unused files

--- compile.R
+++ compile.R
@@ -3,3 +3,13 @@ options(htmlwidgets.TOJSON_ARGS = list(pretty = TRUE), htmltools.dir.version = F
 Sys.setenv(R_KNITR_OPTIONS = 'knitr.chunk.tidy=FALSE')
 library(DT)
 rmarkdown::render(commandArgs(TRUE))
+unlink(list.files('.', '[.]map$', recursive = TRUE))
+unlink(c(
+  'libs/bootstrap/js/npm.js', 'libs/bootstrap/js/bootstrap.js',
+  'libs/bootstrap/css/bootstrap.css', 'libs/bootstrap/css/bootstrap.min.css',
+  'libs/bootstrap/css/bootstrap-theme.css', 'libs/bootstrap/css/bootstrap-theme.min.css',
+  sprintf(
+    'libs/bootstrap/css/%s.min.css',
+    c('cerulean', 'cosmo', 'journal', 'lumen', 'paper', 'readable', 'sandstone', 'simplex', 'spacelab', 'united', 'yeti')
+  )
+))
