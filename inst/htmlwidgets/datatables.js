rstudio/DT

commit bde1bec3fb7cc47908292c371cb5c7caec0673c2

fixes #158: do not notify Shiny if the new table information is the same as the old

inst/htmlwidgets/datatables.js
@@ -521,10 +521,17 @@ HTMLWidgets.widget({
     if (!HTMLWidgets.shinyMode) return;
 
     var methods = {};
+    var shinyData = {}, sameData = function(x, y) {
+      return x === y ? true : JSON.stringify(x) === JSON.stringify(y);
+    };
+
 
     var changeInput = function(id, data, type) {
       id = el.id + '_' + id;
       if (type) id = id + ':' + type;
+      // do not update if the new data is the same as old data
+      if (shinyData.hasOwnProperty(id) && sameData(shinyData[id], data)) return;
+      shinyData[id] = data;
       Shiny.onInputChange(id, data);
     };
 

commit f0eab258510e36246458cc7690681b6444628901

fix #254: store and compare the serialized data instead of the original data
the bug was introduced by bde1bec3fb7cc47908292c371cb5c7caec0673c2 when trying to fix #158: shinyData is a mutable object, and it can be implicitly updated, e.g. when selected1 is changed, shinyData['foo_rows_selected'] can be automatically updated to the new value of selected1; we do not want this to happen, so we store the serialized value so that is not mutable

inst/htmlwidgets/datatables.js
@@ -526,17 +526,15 @@ HTMLWidgets.widget({
     if (!HTMLWidgets.shinyMode) return;
 
     var methods = {};
-    var shinyData = {}, sameData = function(x, y) {
-      return x === y ? true : JSON.stringify(x) === JSON.stringify(y);
-    };
-
+    var shinyData = {};
 
     var changeInput = function(id, data, type) {
       id = el.id + '_' + id;
       if (type) id = id + ':' + type;
       // do not update if the new data is the same as old data
-      if (shinyData.hasOwnProperty(id) && sameData(shinyData[id], data)) return;
-      shinyData[id] = data;
+      if (shinyData.hasOwnProperty(id) && shinyData[id] === JSON.stringify(data))
+        return;
+      shinyData[id] = JSON.stringify(data);
       Shiny.onInputChange(id, data);
     };
 