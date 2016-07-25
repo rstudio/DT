e22729a217d5afb46a9d6054967e11642e64119f
Yihui Xie <xie@yihui.name>
2016-07-25 15:10:27 -0500

    TableTools has been retired

--- R/datatables.R
+++ R/datatables.R
@@ -198,12 +198,6 @@ datatable = function(
     if (identical(options$lengthMenu, c(10, 25, 50, 100)))
       options$lengthMenu = NULL  # that is just the default
   }
-  # if you use copy_csv_xls.swf, we should disable the pdf button in TableTools
-  swf = options$tableTools$sSwfPath
-  if (length(swf) == 1 && length(options$tableTools$aButtons) == 0) {
-    if (basename(swf) == 'copy_csv_xls.swf')
-      options$tableTools$aButtons = c('copy', 'csv', 'xls', 'print')
-  }
 
   # record fillContainer and autoHideNavigation
   if (!is.null(fillContainer)) params$fillContainer = fillContainer
