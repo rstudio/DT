rstudio/DT

commit 37759b5bc4325ec0431d0b52c8cbe9fb20a4f5e5

fix #355: support styling NA's via styleEqual()

R/format.R
@@ -205,11 +205,11 @@ tplStyle = function(cols, valueCols, target, styles, ...) {
   switch(
     target,
     cell = sprintf(
-      "var value=data[%s]; if (value!==null) $(this.api().cell(row, %s).node()).css({%s});",
+      "var value=data[%s]; $(this.api().cell(row, %s).node()).css({%s});",
       valueCols, cols, css
     ),
     row = sprintf(
-      "var value=data[%s]; if (value!==null) $(row).css({%s});",
+      "var value=data[%s]; $(row).css({%s});",
       valueCols, css
     ),
     stop('Invalid target!')
@@ -266,9 +266,13 @@ styleEqual = function(levels, values) {
   if (n != length(values))
     stop("length(levels) must be equal to length(values)")
   if (n == 0) return("''")
+  levels2 = levels
+  if (is.character(levels)) levels2 = gsub("'", "\\'", levels)
+  levels2 = sprintf("'%s'", levels2)
+  levels2[is.na(levels)] = 'null'
   js = ''
   for (i in seq_len(n)) {
-    js = paste0(js, sprintf("value == '%s' ? '%s' : ", levels[i], values[i]))
+    js = paste0(js, sprintf("value == %s ? '%s' : ", levels2[i], values[i]))
   }
   JS(paste0(js, "''"))
 }

commit 74bd6295f53d6a41963d66034348d6e8d2d9f2fe

fix #116 again: handle missing or non-numeric values in styleInterval(), in which case we reset the CSS property, i.e. $el.css(property, '')
the bug was introduced in 37759b5bc4325ec0431d0b52c8cbe9fb20a4f5e5 when fixing #355

DESCRIPTION
@@ -1,7 +1,7 @@
 Package: DT
 Type: Package
 Title: A Wrapper of the JavaScript Library 'DataTables'
-Version: 0.2.16
+Version: 0.2.17
 Date: 2016-08-08
 Authors@R: c(
     person("Yihui", "Xie", email = "xie@yihui.name", role = c("aut", "cre")),

R/format.R
@@ -265,7 +265,7 @@ styleInterval = function(cuts, values) {
   values = sprintf("'%s'", values)
   if (n == 0) return(values)
   if (!all(cuts == sort(cuts))) stop("'cuts' must be sorted increasingly")
-  js = ''
+  js = "isNaN(parseFloat(value)) ? '' : "  # missing or non-numeric values in data
   for (i in seq_len(n)) {
     js = paste0(js, sprintf('value <= %s ? %s : ', cuts[i], values[i]))
   }