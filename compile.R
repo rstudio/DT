315797dbd14ac5e691282fe88f605072586589a1
Yihui Xie <xie@yihui.name>
2018-04-26 11:19:55 -0500

    pandoc 2.x

--- compile.R
+++ compile.R
@@ -6,12 +6,14 @@ o = rmarkdown::render(commandArgs(TRUE), quiet = TRUE)
 x = gsub('\\u003c', '<', readLines(o), fixed = TRUE)
 x = gsub('<\\\\(/[a-z1-6]+>)', '<\\1', x)
 x = blogdown:::clean_widget_html(x)
+x = bookdown:::clean_pandoc2_highlight_tags(x)
 writeLines(x, o)
 unlink(list.files('.', '[.]map$', recursive = TRUE))
 unlink(c(
   'libs/bootstrap/js/npm.js', 'libs/bootstrap/js/bootstrap.js',
   'libs/bootstrap/css/bootstrap.css', 'libs/bootstrap/css/bootstrap.min.css',
   'libs/bootstrap/css/bootstrap-theme.css', 'libs/bootstrap/css/bootstrap-theme.min.css',
+  'libs/datatables-binding/datatables.yaml',
   sprintf(
     'libs/bootstrap/css/%s.min.css',
     c('cerulean', 'cosmo', 'journal', 'lumen', 'paper', 'readable', 'sandstone', 'simplex', 'spacelab', 'united', 'yeti')
