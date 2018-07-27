0bcf6d937b178b79de06dc001b573eae7f8fe442
shrektan <shrektan@126.com>
2018-07-27 09:18:03 +0800

    tweak the js code

--- inst/htmlwidgets/datatables.js
+++ inst/htmlwidgets/datatables.js
@@ -29,8 +29,8 @@ DTWidget.formatCurrency = function(thiz, row, data, col, currency, digits, inter
 DTWidget.formatString = function(thiz, row, data, col, prefix, suffix) {
   var d = data[col];
   if (d === null) return;
-  var content = $(thiz.api().cell(row, col).node()).html();
-  $(thiz.api().cell(row, col).node()).html(prefix + content + suffix);
+  var cell = $(thiz.api().cell(row, col).node());
+  cell.html(prefix + cell.html() + suffix);
 };
 
 DTWidget.formatPercentage = function(thiz, row, data, col, digits, interval, mark, decMark) {
