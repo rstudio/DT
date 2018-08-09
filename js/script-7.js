10357dd7f6dbdd86e7ffdef5fbdce83f505a37e6
Yihui Xie <xie@yihui.name>
2018-08-09 11:35:44 -0500

    much cleaner HTML!
    https://d.cosx.org/d/420117

---
+++ js/script-7.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  window.buildTabsets("TOC");
+});
+
+$(document).ready(function () {
+  $('.tabset-dropdown > .nav-tabs > li').click(function () {
+    $(this).parent().toggleClass('nav-tabs-open')
+  });
+});
---
+++ js/script-8.js
@@ -0,0 +1,7 @@
+// add bootstrap table styles to pandoc tables
+function bootstrapStylePandocTables() {
+  $('tr.header').parent('thead').parent('table').addClass('table table-condensed');
+}
+$(document).ready(function () {
+  bootstrapStylePandocTables();
+});
