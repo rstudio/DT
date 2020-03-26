965e04398b1e59fd0f7a7769d010a733af46aad6
shrektan <shrektan@126.com>
2020-03-27 01:16:04 +0800

    add tests

---
+++ tests/testit/test-format.R
@@ -0,0 +1,13 @@
+library(testit)
+
+# fix issue #785
+assert("formatXXX() should throw clear errors when table is not valid", {
+  # The implementation of formatDate is a little different from other formatting functions
+  # So we test both of them
+  has_error(formatDate(list(x = 1L)), silent = TRUE)
+  has_error(formatCurrency(list(x = 1L)), silent = TRUE)
+  out <- try(formatDate(list(x = 1L)), silent = TRUE)
+  (grepl("Invalid table", attr(out, "condition")$message, fixed = TRUE))
+  out <- try(formatCurrency(list(x = 1L)), silent = TRUE)
+  (grepl("Invalid table", attr(out, "condition")$message, fixed = TRUE))
+})
