5ca1ce79c17830fcc14387d458d48c7f96bdcbf1
shrektan <shrektan@126.com>
2020-03-28 00:46:18 +0800

    add tests for convertIdx() and escapeData()

--- tests/testit/test-datatables.R
+++ tests/testit/test-datatables.R
@@ -21,3 +21,31 @@ assert('classNameDefinedColumns() works', {
   opt = list(columnDefs = list(list(targets = 'bbb', className = 'xxx')))
   (classNameDefinedColumns(opt, ncol = 5L) %==% integer())
 })
+
+assert('convertIdx() works', {
+  (convertIdx(1L, c('a', 'b')) %==% 1L)
+  (convertIdx(1L, c('a', 'b'), invert = TRUE) %==% -1L)
+  (convertIdx(c(TRUE, FALSE), c('a', 'b')) %==% c(TRUE, FALSE))
+  (convertIdx(c(TRUE, FALSE), c('a', 'b'), invert = TRUE) %==% c(FALSE, TRUE))
+  has_error(convertIdx('a', NULL), silent = TRUE)
+  (convertIdx(c('b', 'd'), letters) %==% c(b = 2L, d = 4L))
+  (convertIdx(c('b', 'd'), letters[1:4], invert = TRUE) %==% c(a = 1L, c = 3L))
+  has_error(convertIdx('c', c('a', 'b')), silent = TRUE)
+})
+
+assert('escapeData() works', {
+  # escapeData() is designed to handle only data.frame or NULL
+  # other type of inputs is undefined (may not throw error)
+  (escapeData(NULL, TRUE) %==% NULL)
+  (escapeData(iris, FALSE) %==% iris)
+  (escapeData(character(), TRUE) %==% character())
+  # only escape character and factor
+  data = data.frame(A = '<', B = factor('>'), C = 1, stringsAsFactors = FALSE)
+  expect = data.frame(A = '&lt;', B = '&gt;', C = 1, stringsAsFactors = FALSE)
+  (escapeData(data, TRUE, colnames(data)) %==% expect)
+  # i to control the escape part
+  (escapeData(data, FALSE, colnames(data)) %==% data)
+  expect = data.frame(A = '&lt;', B = factor('>'), C = 1, stringsAsFactors = FALSE)
+  (escapeData(data, c(1, 3), colnames(data)) %==% expect)
+  (escapeData(data, 'A', colnames(data)) %==% expect)
+})
