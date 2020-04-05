f542bb19a4bb06b8bc885f0238f33568823cffba
Xianying Tan <shrektan@126.com>
2020-04-06 03:51:17 +0800

    make `renderDT(server=)` reactive in Shiny (#794)

--- R/shiny.R
+++ R/shiny.R
@@ -77,7 +77,7 @@ renderDataTable = function(
   outputInfoEnv[["session"]] = NULL
 
   exprFunc = shiny::exprToFunction(expr, env, quoted = TRUE)
-  argFunc = shiny::exprToFunction(list(...), env, quoted = FALSE)
+  argFunc = shiny::exprToFunction(list(..., server = server), env, quoted = FALSE)
   widgetFunc = function() {
     opts = options(DT.datatable.shiny = TRUE); on.exit(options(opts), add = TRUE)
     instance = exprFunc()
@@ -88,9 +88,12 @@ renderDataTable = function(
     }
   }
   processWidget = function(instance) {
+    args = argFunc(); args = args[-length(args)] # the last element is `server`
+    # which is only used in `renderDT()` not `datatable()`, the reason
+    # of having it in `argFunc()` is we want `server` to be reactive
     if (!all(c('datatables', 'htmlwidget') %in% class(instance))) {
-      instance = do.call(datatable, c(list(instance), argFunc()))
-    } else if (length(argFunc()) != 0) {
+      instance = do.call(datatable, c(list(instance), args))
+    } else if (length(args) != 0) {
       warning("renderDataTable ignores ... arguments when expr yields a datatable object; see ?renderDataTable")
     }
 
