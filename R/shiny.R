a831161d6a7936db0e66817189f5de896bd178d3
Winston Chang <winston@stdout.org>
2020-11-19 15:56:13 -0600

    Add support for shiny::bindCache() (#876)

--- R/shiny.R
+++ R/shiny.R
@@ -46,7 +46,9 @@ DTOutput = dataTableOutput
 #'   data is kept on the server and the browser requests a page at a time; if
 #'   \code{FALSE}, then the entire data frame is sent to the browser at once.
 #'   Highly recommended for medium to large data frames, which can cause
-#'   browsers to slow down or crash.
+#'   browsers to slow down or crash. Note that if you want to use
+#'   \code{renderDataTable} with \code{\link[shiny]{bindCache}()}, this must be
+#'   \code{FALSE}.
 #' @param funcFilter (for expert use only) passed to the \code{filter} argument
 #'   of \code{\link{dataTableAjax}()}
 #' @param ... ignored when \code{expr} returns a table widget, and passed as
@@ -150,21 +152,42 @@ renderDataTable = function(
     widgetFunc(), dataTableOutput, environment(), FALSE
   )
 
-  func = shiny::markRenderFunction(dataTableOutput, function(shinysession, name, ...) {
-    domain = tempVarsPromiseDomain(outputInfoEnv, outputName = name, session = shinysession)
+  # The cacheHint arg is not present in Shiny < 1.6.0. Once that version is
+  # very widely used, we can remove this if() statement.
+  if ("cacheHint" %in% names(formals(shiny::markRenderFunction))) {
+    # Can't cache with server-side processing
+    if (server) {
+      cacheHint <- FALSE
+    } else {
+      cacheHint <- list(label = "renderDataTable", userExpr = expr)
+    }
 
-    promises::with_promise_domain(domain, renderFunc())
-  })
+    func <- shiny::markRenderFunction(
+      uiFunc = dataTableOutput,
+      renderFunc = function(shinysession, name, ...) {
+        domain = tempVarsPromiseDomain(outputInfoEnv, outputName = name, session = shinysession)
 
-  # This snapshotPreprocessOutput function was added in shiny 1.0.3.9002
-  if (exists("snapshotPreprocessOutput", asNamespace("shiny"))) {
-    func = shiny::snapshotPreprocessOutput(func, function(value) {
-      # Looks for a string like this in the JSON:
-      # "url":"session/2a2b834d90637a7559f3ebaba460ad10/dataobj/table?w=&nonce=aea032f33aedfd0e",
-      # and removes it, so that the value isn't saved in test snapshots.
-      gsub('"ajax"\\s*:\\s*\\{\\s*"url"\\s*:\\s*"[^"]*"\\s*,?', '"ajax":{', value)
-    })
+        promises::with_promise_domain(domain, renderFunc())
+      },
+      cacheHint = cacheHint
+    )
+  } else {
+    func <- shiny::markRenderFunction(
+      uiFunc = dataTableOutput,
+      renderFunc = function(shinysession, name, ...) {
+        domain = tempVarsPromiseDomain(outputInfoEnv, outputName = name, session = shinysession)
+
+        promises::with_promise_domain(domain, renderFunc())
+      }
+    )
   }
+
+  func = shiny::snapshotPreprocessOutput(func, function(value) {
+    # Looks for a string like this in the JSON:
+    # "url":"session/2a2b834d90637a7559f3ebaba460ad10/dataobj/table?w=&nonce=aea032f33aedfd0e",
+    # and removes it, so that the value isn't saved in test snapshots.
+    gsub('"ajax"\\s*:\\s*\\{\\s*"url"\\s*:\\s*"[^"]*"\\s*,?', '"ajax":{', value)
+  })
 
   shiny::registerInputHandler('DT.cellInfo', function(val, ...) {
     opts = options(stringsAsFactors = FALSE); on.exit(options(opts), add = TRUE)
