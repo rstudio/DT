ca57655b061378e6c2faf005052671172b584682
Carson Sievert <cpsievert1@gmail.com>
2022-10-26 14:02:06 -0500

    Add `fill` argument to `dataTableOutput()` (#1027)
    * Revert "dataTableOutput()'s width and height now default to NULL  (#1026)"
    
    This reverts commit e02783c573b58ccc7bc000e574a3aed748b4ba28.
    
    * Add fill argument to dataTableOutput()

--- R/shiny.R
+++ R/shiny.R
@@ -7,6 +7,8 @@
 #' @inheritParams shiny::dataTableOutput
 #' @param width the width of the table container
 #' @param height the height of the table container
+#' @param fill passed to \code{htmlwidgets::\link{shinyWidgetOutput}()}, see
+#'   there for explanation (requires \pkg{htmlwidgets} > v1.5.4).
 #' @references \url{https://rstudio.github.io/DT/shiny.html}
 #' @export
 #' @examples # !formatR
@@ -22,11 +24,13 @@
 #'     }
 #'   )
 #' }
-dataTableOutput = function(outputId, width = NULL, height = NULL) {
+dataTableOutput = function(outputId, width = '100%', height = 'auto', fill = TRUE) {
+  args = list(outputId, 'datatables', width, height, package = 'DT')
+  if ("fill" %in% names(formals(htmlwidgets::shinyWidgetOutput)))
+    args$fill = fill
+
   htmltools::attachDependencies(
-    htmlwidgets::shinyWidgetOutput(
-      outputId, 'datatables', width, height, package = 'DT'
-    ),
+    do.call(htmlwidgets::shinyWidgetOutput, args),
     crosstalk::crosstalkLibs(),
     append = TRUE
   )
