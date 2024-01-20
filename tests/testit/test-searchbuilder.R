8c2460938938c81b1721597f8f8bc29fdb14511d
Mikko Marttila <13412395+mikmart@users.noreply.github.com>
2024-01-20 08:59:13 +0000

    Streamline tests

--- tests/testit/test-searchbuilder.R
+++ tests/testit/test-searchbuilder.R
@@ -5,6 +5,7 @@ assert('SearchBuilder condition evaluation works', {
   (sbEvaluateCondition('between', 'num', 7, c(2, 4)) == FALSE)
   (sbEvaluateCondition('starts', 'string', 'foo', 'f') == TRUE)
   (sbEvaluateCondition('starts', 'string', factor('foo'), 'f') == TRUE)
+  (sbEvaluateCondition('null', 'string', c('', NA)) == c(TRUE, TRUE))
 })
 
 assert('SearchBuilder logic evaluation works', {
@@ -19,17 +20,6 @@ assert('SearchBuilder logic evaluation works', {
     data.frame(a = 1:9)
   )  
   (setequal(which(res), 2:4))
-  res = sbEvaluateSearch(
-    list(
-      logic = 'OR',
-      criteria = list(
-        list(condition = '>', data = 'a', value = '4', type = 'num'),
-        list(condition = '<', data = 'a', value = '2', type = 'num')
-      )
-    ),
-    data.frame(a = 1:9)
-  )  
-  (setequal(which(res), c(1, 5:9)))
 })
 
 assert('SearchBuilder complex queries work', {
